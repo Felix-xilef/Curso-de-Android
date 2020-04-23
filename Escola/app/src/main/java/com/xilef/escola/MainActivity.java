95ecb26c6d2a75d67c6431b5087a77ee182618b0
Felix <felixpb@yahoo.com.br>
2020-04-22 21:46:46 -0300

    Escola v1.0 (1)
    Criação e implementação de parte do app Escola v1.0 no Android Studio (M02A00-07)

---
+++ Escola/app/src/main/java/com/xilef/escola/MainActivity.java
@@ -0,0 +1,59 @@
+package com.xilef.escola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtN1, edtN2;
+    TextView txtM, txtSit;
+    LinearLayout layResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtN1 = findViewById(R.id.edtN1);
+        edtN2 = findViewById(R.id.edtN2);
+        txtM = findViewById(R.id.txtM);
+        txtSit = findViewById(R.id.txtSit);
+        layResult = findViewById(R.id.layResult);
+
+    }
+
+    public void calcular(View view) {
+        boolean ok = true;
+        if (edtN1.getText().toString().trim().isEmpty()) {
+            ok = false;
+            edtN1.setError(getString(R.string.msgErro));
+        }
+        if (edtN2.getText().toString().trim().isEmpty()) {
+            ok = false;
+            edtN2.setError(getString(R.string.msgErro));
+        }
+        if (ok) {
+            float n1 = Float.parseFloat(edtN1.getText().toString()), n2 = Float.parseFloat(edtN2.getText().toString());
+            float m = (n1 + n2) / 2;
+
+            txtM.setText(String.format("%.1f", m));
+
+            if (m < 5) {
+                txtSit.setText(getString(R.string.strSitRp));
+                txtSit.setTextColor(getResources().getColor(R.color.corReprovado));
+            } else if (m < 7) {
+                txtSit.setText(getString(R.string.strSitRc));
+                txtSit.setTextColor(getResources().getColor(R.color.corRecuperacao));
+            } else {
+                txtSit.setText(getString(R.string.strSitAp));
+                txtSit.setTextColor(getResources().getColor(R.color.corAprovado));
+            }
+        }
+    }
+}
