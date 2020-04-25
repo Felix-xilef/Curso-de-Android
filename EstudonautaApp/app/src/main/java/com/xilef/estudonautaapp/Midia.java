2f4e559adbecca3764420902bec0a2049b6eb55d
Felix <felixpb@yahoo.com.br>
2020-04-25 19:52:00 -0300

    Estudonauta App (1)
    Criação e implementação de parte do app Estudonauta App no Android Studio (M03A00-06)

---
+++ EstudonautaApp/app/src/main/java/com/xilef/estudonautaapp/Midia.java
@@ -0,0 +1,19 @@
+package com.xilef.estudonautaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Midia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_midia);
+    }
+
+    public void clickVoltar(View view) {
+        finish();
+    }
+}
